bfdbbfdb723cb4eaabd17d4dc687090161abcb46
Alberto Arroyo <albertoarroyo@hotmail.es>
2024-01-12 15:32:57 +0100

    Firebase

--- Android_Studio/app/src/main/java/com/example/todolist/Login.java
+++ Android_Studio/app/src/main/java/com/example/todolist/Login.java
@@ -2,13 +2,46 @@ package com.example.todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
+    //variable para guardar los botones
+    Button botonLogin;
+    TextView botonRegistro;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        botonLogin = findViewById(R.id.botonLogin);
+        //poner a la escucha
+        botonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Para que pase del login a la main
+                Intent intent = new Intent(Login.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        botonRegistro = findViewById(R.id.botonRegistro);
+        botonRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //crear el usuario en firebase
+
+                //Si es correcto saco un toast y paso a la siguiente
+                Toast.makeText(Login.this, "Usuario Registrado", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(Login.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- Android_Studio/app/src/main/java/com/example/todolist/MainActivity.java
+++ Android_Studio/app/src/main/java/com/example/todolist/MainActivity.java
@@ -1,14 +1,67 @@
 package com.example.todolist;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //getSupportActionBar().show();
+    }
+    //Para que aparezca el menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    //doy funcionalidad a cada uno de los botones
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId()==R.id.mas){
+            final EditText taskEditext = new EditText(this);
+            AlertDialog dialog = new AlertDialog.Builder(this)
+                    .setTitle("Nueva tarea")
+                    .setMessage("Que quieres hacer a continuación")
+                    .setView(taskEditext)
+                    .setPositiveButton("Añadir", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            //añadir la tarea a la base de datos
+                        }
+                    })
+                    .setNegativeButton("Cancelar", null)
+                    .create();
+            //activar el cuadro de dialogo para añadir tarea
+            Toast.makeText(this, "Tarea añadida", Toast.LENGTH_LONG).show();
+            dialog.show();
+            return true;
+        }
+        if (item.getItemId()==R.id.logout){
+            //cierre de sesion
+
+            //pasar al login al cerrar sesion
+            startActivity(new Intent(MainActivity.this, Login.class));
+            return true;
+        }
+        else{
+            return super.onOptionsItemSelected(item);
+        }
+
     }
 }
