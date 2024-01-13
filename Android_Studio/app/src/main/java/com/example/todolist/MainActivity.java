e3ba21b6f58d3a54107b9665a7e71e34825c1951
Alberto Arroyo <albertoarroyo@hotmail.es>
2024-01-13 11:00:41 +0100

    Altas y consultas de las listas

--- Android_Studio/app/src/main/java/com/example/todolist/MainActivity.java
+++ Android_Studio/app/src/main/java/com/example/todolist/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.todolist;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
@@ -9,15 +10,38 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 import android.widget.Toolbar;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+
+    //Variables para altas y consultas de firebase
+    FirebaseFirestore miBaseDatos;
+    String idUser;
+
+    ListView listViewTareas;
+    List<String> listaTareas = new ArrayList<>();
+    List<String> listaIdTareas = new ArrayList<>();
+    ArrayAdapter<String> mAdapterTareas;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +50,14 @@ public class MainActivity extends AppCompatActivity {
 
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
+
+        //Inicializo variables para altas y consultas
+        miBaseDatos = FirebaseFirestore.getInstance();
+        idUser = mAuth.getCurrentUser().getUid();
+
+        listViewTareas = findViewById(R.id.ListView);
+
+        actualizarUI();
     }
     //Para que aparezca el menu
     @Override
@@ -49,12 +81,21 @@ public class MainActivity extends AppCompatActivity {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
                             //añadir la tarea a la base de datos
+                            String tarea = taskEditext.getText().toString();
+                            Map<String, Object> miTarea = new HashMap<>();
+                            miTarea.put("nombreTarea", tarea);
+                            miTarea.put("idUsuario", idUser);
+
+                            //Añadir un nuevo documento con el id generado
+                            miBaseDatos.collection("Tareas").add(miTarea);
+                            Toast.makeText(MainActivity.this, "Tarea añadida", Toast.LENGTH_LONG).show();
+
                         }
                     })
                     .setNegativeButton("Cancelar", null)
                     .create();
             //activar el cuadro de dialogo para añadir tarea
-            Toast.makeText(this, "Tarea añadida", Toast.LENGTH_LONG).show();
+
             dialog.show();
             return true;
         }
@@ -71,4 +112,39 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+    //Metodo para actualizar los datos en tiempo real de la coleccion
+
+    private void actualizarUI(){
+        miBaseDatos.collection("Tareas")
+                .whereEqualTo("idUsuario", idUser) //Solo las tareas del usuario logeado
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException e) {
+                        if (e != null){
+                            return;
+                        }
+
+                        //limpiar las listas
+                        listaIdTareas.clear();
+                        listaTareas.clear();
+
+                        //rellenar el listview con las listas
+                        for(QueryDocumentSnapshot doc: value){
+                            listaIdTareas.add(doc.getId());
+                            listaTareas.add(doc.getString("nombreTarea"));
+
+                        }
+
+                        //si la lista esta vacia
+                        if(listaTareas.size()==0){
+                            listViewTareas.setAdapter(null);
+                        }else{
+                            mAdapterTareas = new ArrayAdapter<>(MainActivity.this,R.layout.item_tarea, R.id.textViewTarea, listaTareas);
+                            listViewTareas.setAdapter(mAdapterTareas);
+                        }
+                    }
+                });
+
+    }
+
 }
